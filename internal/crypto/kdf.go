7bc1bb4cb6fcfec8fb25f3f0865ed22f7b4b884f
thealienbit <thealienbit@outlook.com>
2026-02-14 16:19:43 +0530

    kdf, added documentation

--- internal/crypto/kdf.go
+++ internal/crypto/kdf.go
@@ -1,3 +1,32 @@
+/*
+* KDF - Key Derivation Function
+*
+* Using Argon2id for KDF
+*
+* Argon2i: (Interactive) Designed to resist side channel attacks, accesses memory
+* in a password independent order, safe for situations where attacker 
+* might be able to monitor memory timeing
+*
+* Argon2d: (Data-Dependent) Designed to resist GPU/ASIC cracking attacks, accesses
+* memory in a password dependent order , which makes is harder to guess password 
+* using hardware
+*
+* Argon2id: Uses best of both worlds. Uses first pass over memory from Argon2i and
+* subsequent passes using the Argon2d
+*
+* Why not traditional algorithms like SHA256 ?
+* SHA256 is extremely fast, which is a disadvantage for password storage because,
+* attacker with a powerful GPU can guess billions of SHA256 hashes per second
+* Argon2id fights this with three main "cost factors
+* 1) memory hardness: You can specify exactly how much RAM the algorithm must use
+* 	(e.g., 64MB or 1GB). This makes it incredibly expensive for attackers to build
+* 	custom hardware to crack passwords at scale
+* 2) Time Cost ($t$): You can define how many iterations the algorithm performs, 
+* 	forcing a delay that frustrates "brute-force" attempts
+* 3) Parallelism ($p$): It can be tuned to use multiple CPU cores, 
+* 	allowing legitimate servers to compute the hash quickly while still remaining 
+* 	"heavy" for an attacker
+*/
 package crypto
 
 import (
